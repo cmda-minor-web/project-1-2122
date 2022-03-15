24200f25dcaaa1b5f7281e4793248b17521f0611
AronPelgrim <aron.pelgrim@hva.nl>
2022-03-15 17:12:26 +0100

    Added styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 const cors = 'https://cors-anywhere.herokuapp.com/'
 const search = 'https://zoeken.oba.nl/api/v1/search/?q='
 const key = 'dc0e2f073c03758140452044906bc818'
-const categorie = 'voeding'
+const categorie = 'sportvoeding'
 const secret = '6b02878111c06660300798cf4c40a685'
 const config = {
     Authorization: `Bearer ${secret}`
