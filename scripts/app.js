68e7f14be15df277f7a4c5fc78df021d5c21e5c1
AronPelgrim <aron.pelgrim@hva.nl>
2022-03-16 11:12:38 +0100

    Added js

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,14 @@
 const cors = 'https://cors-anywhere.herokuapp.com/'
 const search = 'https://zoeken.oba.nl/api/v1/search/?q='
 const key = 'dc0e2f073c03758140452044906bc818'
-const categorie = 'sportvoeding'
 const secret = '6b02878111c06660300798cf4c40a685'
 const config = {
     Authorization: `Bearer ${secret}`
 }
-const url = `${cors}${search}${categorie}&authorization=${key}&sort=act_dt_asc&output=json`
+
+const valueDropdown = dropDown.options[dropDown.selectedIndex].value.toString();
+
+const url = `${cors}${search}${valueDropdown}&authorization=${key}&sort=act_dt_asc&output=json`
 
 fetch(url, config)
     .then(response => response.json())
